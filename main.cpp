46797d37ee0b65cdf41cc5159129e1e7cc7edaba
sklbz <sokol.bozanic@tumo.world>
2024-06-12 18:43:20 +0200

    added variable for size

--- main.cpp
+++ main.cpp
@@ -5,18 +5,18 @@
 int k;
 double sin(), cos();
 int main() {
-  float A = 0, B = 0, i, j, z[1760];
+  float A = 0, B = 0, i, j, z[1760], mess_modif = 0.5f;
   char b[1760];
   printf("\x1b[2J");
-  for (;;) {
+  while (1) {
     memset(b, 32, 1760);
     memset(z, 0, 7040);
     for (j = 0; 6.28 > j; j += 0.07) {
       for (i = 0; 6.28 > i; i += 0.02) {
         float sini = sin(i), cosj = cos(j), sinA = sin(A), sinj = sin(j),
               cosA = cos(A), cosj2 = cosj + 2,
-              mess = 1 / (sini * cosj2 * sinA + sinj * cosA + 5), cosi = cos(i),
-              cosB = cos(B), sinB = sin(B),
+              mess = mess_modif / (sini * cosj2 * sinA + sinj * cosA + 5),
+              cosi = cos(i), cosB = cos(B), sinB = sin(B),
               t = sini * cosj2 * cosA - sinj * sinA;
         int x = 40 + 30 * mess * (cosi * cosj2 * cosB - t * sinB),
             y = 12 + 15 * mess * (cosi * cosj2 * sinB + t * cosB),
