3123e1628e495d4cede1605ec604a6d6d49341a9
sklbz <sokol.bozanic@tumo.world>
2024-06-12 18:49:21 +0200

    update

--- main.cpp
+++ main.cpp
@@ -6,14 +6,13 @@ int k;
 double sin(), cos();
 int main() {
   float A = 0, B = 0, i, j, z[1760], size = 2, t_modifier = 1;
-  float test = 0.7;
   char b[1760];
   printf("\x1b[2J");
   while (1) {
     memset(b, 32, 1760);
     memset(z, 0, 7040);
-    for (j = 0; test > j; j += 0.07) {
-      for (i = 0; test > i; i += 0.02) {
+    for (j = 0; 6.28 > j; j += 0.07) {
+      for (i = 0; 6.28 > i; i += 0.02) {
         float sini = sin(i), cosj = cos(j), sinA = sin(A), sinj = sin(j),
               cosA = cos(A), cosj2 = cosj + 2,
               mess = size / (sini * cosj2 * sinA + sinj * cosA + 5),
